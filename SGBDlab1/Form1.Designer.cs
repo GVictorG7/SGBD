09cfdc8c91906f6fd75f03d2609e01721eecbf8e
Victor <victor.georgescu07@gmail.com>
2018-05-30 16:44:27 +0300

    initial commit

--- SGBDlab1/Form1.Designer.cs
+++ SGBDlab1/Form1.Designer.cs
@@ -23,13 +23,137 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.components = new System.ComponentModel.Container();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.dataGridView2 = new System.Windows.Forms.DataGridView();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AccessibleName = "DataGridViewParent";
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(404, 197);
+			this.dataGridView1.TabIndex = 0;
+			// 
+			// dataGridView2
+			// 
+			this.dataGridView2.AccessibleName = "DataGridViewChild";
+			this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView2.Location = new System.Drawing.Point(12, 268);
+			this.dataGridView2.Name = "dataGridView2";
+			this.dataGridView2.Size = new System.Drawing.Size(404, 170);
+			this.dataGridView2.TabIndex = 1;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(477, 335);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 10;
+			this.button1.Text = "Add";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(587, 335);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 11;
+			this.button2.Text = "Delete";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(698, 335);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 12;
+			this.button3.Text = "Update";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(477, 415);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 23);
+			this.button4.TabIndex = 13;
+			this.button4.Text = "Refresh";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(698, 415);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(75, 23);
+			this.button5.TabIndex = 14;
+			this.button5.Text = "Next";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			// 
+			// button6
+			// 
+			this.button6.Location = new System.Drawing.Point(587, 414);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(75, 23);
+			this.button6.TabIndex = 15;
+			this.button6.Text = "Previous";
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.button6_Click);
+			// 
+			// panel1
+			// 
+			this.panel1.Location = new System.Drawing.Point(422, 5);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(375, 324);
+			this.panel1.TabIndex = 18;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.dataGridView2);
+			this.Controls.Add(this.dataGridView1);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.DataGridView dataGridView2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Panel panel1;
 	}
 }
 
--- SGBDlab1/Form1.cs
+++ SGBDlab1/Form1.cs
@@ -1,17 +1,204 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
 
-namespace SGBDlab1 {
-	public partial class Form1 : Form {
-		public Form1() {
+namespace SGBDlab1
+{
+	public partial class Form1 : Form
+	{
+		SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["cn"].ConnectionString);
+		SqlDataAdapter adapter = new SqlDataAdapter();
+		DataSet data = new DataSet();
+		BindingSource bs = new BindingSource();
+		SqlDataAdapter adapter2 = new SqlDataAdapter();
+		DataSet data2 = new DataSet();
+		BindingSource bs2 = new BindingSource();
+		string ChildTableName = ConfigurationManager.AppSettings["ChildTableName"];
+		string ChildColumnNames = ConfigurationManager.AppSettings["ChildColumnNames"];
+
+		public Form1()
+		{
 			InitializeComponent();
+		}
+
+		// Add button
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string conn2 = ConfigurationManager.ConnectionStrings["cn"].ConnectionString;
+			SqlConnection conn = new SqlConnection(conn2);
+			try
+			{
+				string ColumnNamesInsertParameters = ConfigurationManager.AppSettings["ColumnNamesInsertParameters"];
+				List<string> ColumnNamesList = new List<string>(ConfigurationManager.AppSettings["ChildColumnNames"].Split(','));
+				adapter.InsertCommand = new SqlCommand("INSERT INTO " + ChildTableName + " VALUES(" + ColumnNamesInsertParameters + ")", conn);
+				foreach (string column in ColumnNamesList)
+				{
+					TextBox textBox = (TextBox)panel1.Controls[column];
+					adapter.InsertCommand.Parameters.AddWithValue("@" + column, textBox.Text);
+				}
+				conn.Open();
+				adapter.InsertCommand.ExecuteNonQuery();
+				MessageBox.Show("Inserted Succesfull to the Database");
+				conn.Close();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+				conn.Close();
+			}
+		}
+
+		// Update button
+		private void button3_Click(object sender, EventArgs e)
+		{
+			string conn2 = ConfigurationManager.ConnectionStrings["cn"].ConnectionString;
+			SqlConnection conn = new SqlConnection(conn2);
+			int x;
+			string UpdateQuery = ConfigurationSettings.AppSettings["UpdateQuery"];
+			adapter.UpdateCommand = new SqlCommand(UpdateQuery, conn);
+			List<string> ColumnNamesList = new List<string>(ConfigurationManager.AppSettings["ChildColumnNames"].Split(','));
+			foreach (string column in ColumnNamesList)
+			{
+				TextBox textBox = (TextBox)panel1.Controls[column];
+				adapter.UpdateCommand.Parameters.AddWithValue("@" + column, textBox.Text);
+			}
+			conn.Open();
+			x = adapter.UpdateCommand.ExecuteNonQuery();
+			conn.Close();
+			if (x >= 1)
+			{
+				MessageBox.Show("The record has been updated");
+			}
+		}
+
+		// Delete button
+		private void button2_Click(object sender, EventArgs e)
+		{
+			string conn2 = ConfigurationManager.ConnectionStrings["cn"].ConnectionString;
+			SqlConnection conn = new SqlConnection(conn2);
+			DialogResult dr;
+			dr = MessageBox.Show("Are you sure?\n No undo after delete", "Confirm Deletion", MessageBoxButtons.YesNo);
+			if (dr == DialogResult.Yes)
+			{
+				string delete = ConfigurationSettings.AppSettings["delete"];
+				adapter.DeleteCommand = new SqlCommand(delete, conn);
+				List<string> ColumnNamesList = new List<string>(ConfigurationManager.AppSettings["ChildColumnNames"].Split(','));
+				TextBox textBox = (TextBox)panel1.Controls[ColumnNamesList[0]];
+				adapter.DeleteCommand.Parameters.AddWithValue("@" + ColumnNamesList[0], textBox.Text);
+				conn.Open();
+				adapter.DeleteCommand.ExecuteNonQuery();
+				conn.Close();
+				data2.Clear();
+				adapter.Fill(data2);
+			}
+			else
+			{
+				MessageBox.Show("Deletion Aborded");
+			}
+		}
+
+		// Refresh button
+		private void button4_Click(object sender, EventArgs e)
+		{
+			string select = ConfigurationSettings.AppSettings["select"];
+			adapter.SelectCommand = new SqlCommand(select, conn);
+			data.Clear();
+			adapter.Fill(data);
+			dataGridView1.DataSource = data.Tables[0];
+			bs.DataSource = data.Tables[0];
+			string select2 = ConfigurationSettings.AppSettings["selectChild"];
+			adapter2.SelectCommand = new SqlCommand(select2, conn);
+			adapter2.SelectCommand.Parameters.Add(ConfigurationManager.AppSettings["legatura"], SqlDbType.SmallInt).Value = 1;
+			data2.Clear();
+			adapter2.Fill(data2);
+			dataGridView2.DataSource = data2.Tables[0];
+			bs2.DataSource = data2.Tables[0];
+			GenerateTexts();
+		}
+
+		private void DataGridViewUpdate()
+		{
+			string conn2 = ConfigurationManager.ConnectionStrings["cn"].ConnectionString;
+			SqlConnection conn = new SqlConnection(conn2);
+			dataGridView1.ClearSelection();
+			dataGridView1.Rows[bs.Position].Selected = true;
+			dataGridView2.ClearSelection();
+			dataGridView2.Rows[bs2.Position].Selected = true;
+			string select2 = ConfigurationSettings.AppSettings["selectChild"];
+			adapter2.SelectCommand = new SqlCommand(select2, conn);
+			adapter2.SelectCommand.Parameters.Add(ConfigurationManager.AppSettings["legatura"], SqlDbType.SmallInt).Value = Int32.Parse(panel1.Controls[ConfigurationManager.AppSettings["legatura"]].Text);
+			data2.Clear();
+			adapter2.Fill(data2);
+			dataGridView2.DataSource = data2.Tables[0];
+			bs2.DataSource = data2.Tables[0];
+		}
+
+		// Next button
+		private void button5_Click(object sender, EventArgs e)
+		{
+			bs.MoveNext();
+			DataGridViewUpdate();
+		}
+
+		// Previous button
+		private void button6_Click(object sender, EventArgs e)
+		{
+			bs.MovePrevious();
+			DataGridViewUpdate();
+		}
+
+		private void GenerateTexts()
+		{
+			int cLeft = 10;
+			List<string> ColumnNamesList = new List<string>(ConfigurationManager.AppSettings["ChildColumnNames"].Split(','));
+			try
+			{
+				foreach (string column in ColumnNamesList)
+				{
+					TextBox textBox = AddNewTextBox(cLeft, column);
+					AddNewLabel(cLeft, column);
+					cLeft += 5;
+					if (column == ConfigurationManager.AppSettings["legatura"])
+					{
+						textBox.DataBindings.Add("Text", bs, column);
+					}
+					else
+					{
+						textBox.DataBindings.Add("Text", bs2, column);
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+				conn.Close();
+			}
+		}
+
+		public TextBox AddNewTextBox(int cleft, string text)
+		{
+			TextBox txt = new TextBox();
+			txt.Name = text;
+			panel1.Controls.Add(txt);
+			txt.Top = cleft * 10;
+			txt.Left = 100;
+			txt.MaxLength = 30;
+			txt.Text = "TextBox" + text;
+			txt.Size = new System.Drawing.Size(142, 27);
+			return txt;
+		}
+
+		public Label AddNewLabel(int cleft, string text)
+		{
+			Label label = new Label();
+			panel1.Controls.Add(label);
+			label.Top = cleft * 10;
+			label.Left = 15;
+			label.Text = text;
+			return label;
 		}
 	}
 }
